1a191496b4413ff04c6fd55aa91cfa15f48b83ee
sasasan03 <sako.hiro62@gmail.com>
2025-12-31 21:51:04 +0900

    layoutsファイル追加

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,4 @@
-@extends('main')
+@extends('layouts.main')
 
 @push('styles')
 <style>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-// Route::get('/', function () {
-//     return view('index');
-// });
-
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/products/{product}', [HomeController::class, 'show'])->name('products.show');
